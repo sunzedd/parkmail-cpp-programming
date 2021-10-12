3b46edde85cdd716f5d63af8599205638ddc3a89
sunzedd <sunz89@yandex.ru>
2021-10-12 22:09:57 +0300

    Add dummy tests for parsetag library

---
+++ homework_1/parsetag/tests/test_string_array.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "string_array.h"
+}
+
+TEST(SplitStringUnitTest, Dummy) {
+    EXPECT_EQ(1, 1);
+}
---
+++ homework_1/parsetag/tests/test_tag.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "tag.h"
+}
+
+TEST(TagCreateUnitTest, Dummy) {
+    EXPECT_EQ(1, 1);
+}
