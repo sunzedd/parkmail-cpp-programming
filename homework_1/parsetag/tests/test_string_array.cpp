bced8991415e5405c472b2f01410e2eb570ffa32
sunzedd <sunz89@yandex.ru>
2021-10-13 00:47:18 +0300

    Update tests

--- homework_1/parsetag/tests/test_string_array.cpp
+++ homework_1/parsetag/tests/test_string_array.cpp
@@ -4,6 +4,96 @@ extern "C" {
 #include "string_array.h"
 }
 
-TEST(SplitStringUnitTest, Dummy) {
-    EXPECT_EQ(1, 1);
+TEST(SPLIT_STRING, EMPTY_INPUT_STRING) {
+    // Arrange
+    const char* const input_str = "";
+    int err = 0;
+    int str_count = 0;
+    const char* const delimeters = "<>/,";
+
+    // Act
+    char** str_array = split_string(&err, &str_count, input_str, delimeters);
+
+    // Assert
+    EXPECT_EQ(str_count, 0);
+    EXPECT_TRUE(str_array == NULL);
+    EXPECT_EQ(err, ERR_INVALID_ARGS);
+
+    destroy_string_array(str_array, str_count);
+}
+
+TEST(SPLIT_STRING, NO_DELIMETERS_IN_INPUT_STRING) {
+    // Arrange
+    const char* const input_str = "hello world";
+    int err = 0;
+    int str_count = 0;
+    const char* const delimeters = "<>/,";
+
+    // Act
+    char** str_array = split_string(&err, &str_count, input_str, delimeters);
+
+    // Assert
+    EXPECT_EQ(str_count, 1);
+    EXPECT_TRUE(str_array != NULL);
+    EXPECT_EQ(err, OK);
+    EXPECT_STREQ(str_array[0], input_str);
+
+    destroy_string_array(str_array, str_count);
+}
+
+TEST(SPLIT_STRING, INPUT_STRING_CONTAIN_ONLY_DELIMETERS) {
+    // Arrange
+    const char* const input_str = "<<<<>>>>//,,,<<,>";
+    int err = 0;
+    int str_count = 0;
+    const char* const delimeters = "<>/,";
+
+    // Act
+    char** str_array = split_string(&err, &str_count, input_str, delimeters);
+
+    // Assert
+    EXPECT_EQ(str_count, 0);
+    EXPECT_TRUE(str_array == NULL);
+    EXPECT_EQ(err, OK);
+}
+
+TEST(SPLIT_STRING, MULTIPLE_TOKENS) {
+    // Arrange
+    const char* const input_str = "<img source=\"somepic.jpg\" width=\"800\" height=\"600\">";
+    int err = 0;
+    int str_count = 0;
+    const char* const delimeters = " <>";
+
+    // Act
+    char** str_array = split_string(&err, &str_count, input_str, delimeters);
+
+    // Assert
+    EXPECT_EQ(str_count, 4);
+    EXPECT_TRUE(str_array != NULL);
+    EXPECT_STREQ(str_array[0], "img");
+    EXPECT_STREQ(str_array[1], "source=\"somepic.jpg\"");
+    EXPECT_STREQ(str_array[2], "width=\"800\"");
+    EXPECT_STREQ(str_array[3], "height=\"600\"");
+    EXPECT_EQ(err, OK);
+
+    destroy_string_array(str_array, str_count);
+}
+
+TEST(SPLIT_STRING, SINGLE_TOKEN) {
+    // Arrange
+    const char* const input_str = "<br>";
+    int err = 0;
+    int str_count = 0;
+    const char* const delimeters = " <>";
+
+    // Act
+    char** str_array = split_string(&err, &str_count, input_str, delimeters);
+
+    // Assert
+    EXPECT_EQ(str_count, 1);
+    EXPECT_TRUE(str_array != NULL);
+    EXPECT_STREQ(str_array[0], "br");
+    EXPECT_EQ(err, OK);
+
+    destroy_string_array(str_array, str_count);
 }
--- homework_1/parsetag/tests/test_tag.cpp
+++ homework_1/parsetag/tests/test_tag.cpp
@@ -4,6 +4,141 @@ extern "C" {
 #include "tag.h"
 }
 
-TEST(TagCreateUnitTest, Dummy) {
-    EXPECT_EQ(1, 1);
+
+TEST(ATTRIBUTE_CONSTRUCT_FROM_HTML, ATTRIBUTE_HAS_VALUE) {
+    // Arrange
+    int err = OK;
+    const char* const html = "source=\"somepic.jpg\"";
+
+    // Act
+    attribute a = attribute_construct_from_html(&err, html);
+
+    // Assert
+    EXPECT_EQ(err, OK);
+    EXPECT_TRUE(a.name != NULL);
+    EXPECT_TRUE(a.value != NULL);
+    EXPECT_STREQ(a.name, "source");
+    EXPECT_STREQ(a.value, "somepic.jpg");
+
+    attribute_destroy_internals(&a);
 }
+
+TEST(ATTRIBUTE_CONSTRUCT_FROM_HTML, ATTRIBUTE_HAS_NO_VALUE) {
+    // Arrange
+    int err = OK;
+    const char* const html = "hidden";
+
+    // Act
+    attribute a = attribute_construct_from_html(&err, html);
+
+    // Assert
+    EXPECT_EQ(err, OK);
+    EXPECT_TRUE(a.name != NULL);
+    EXPECT_TRUE(a.value == NULL);
+    EXPECT_STREQ(a.name, "hidden");
+
+    attribute_destroy_internals(&a);
+}
+
+
+TEST(TAG_VALIDATE_HTML_STR, VALID_HTML) {
+    // Arrange
+    const char* const tag = "<br>";
+
+    // Act
+    int err = tag_validate_html_str(tag);
+
+    // Assert
+    EXPECT_EQ(err, OK);
+}
+
+TEST(TAG_VALIDATE_HTML_STR, INVALID_HTML) {
+    // Arrange
+    const char* const tag = "<br?";
+
+    // Act
+    int err = tag_validate_html_str(tag);
+
+    // Assert
+    EXPECT_EQ(err, ERR_INVALID_HTML_SYNTAX);
+}
+
+TEST(TAG_CREATE_FROM_HTML, VALID_TAG_OPENING) {
+    // Arrange
+    int err = OK;
+    const char* const html = "<br>";
+
+    // Act
+    tag* tag = tag_create_from_html(&err, html);
+
+    // Assert
+    EXPECT_TRUE(tag != NULL);
+    EXPECT_TRUE(tag->name != NULL);
+    EXPECT_STREQ(tag->name, "br");
+    EXPECT_EQ(tag->type, TAG_TYPE_OPENING);
+
+    tag_destroy(tag);
+}
+
+TEST(TAG_CREATE_FROM_HTML, VALID_TAG_CLOSING) {
+    // Arrange
+    int err = OK;
+    const char* const html = "</br>";
+
+    // Act
+    tag* tag = tag_create_from_html(&err, html);
+
+    // Assert
+    EXPECT_TRUE(tag != NULL);
+    EXPECT_TRUE(tag->name != NULL);
+    EXPECT_STREQ(tag->name, "br");
+    EXPECT_EQ(tag->type, TAG_TYPE_CLOSING);
+
+    tag_destroy(tag);
+}
+
+TEST(TAG_CREATE_FROM_HTML, VALID_TAG_NO_ATTRIBUTES) {
+    // Arrange
+    int err = OK;
+    const char* const html = "<br>";
+
+    // Act
+    tag* tag = tag_create_from_html(&err, html);
+
+    // Assert
+    EXPECT_TRUE(tag != NULL);
+    EXPECT_TRUE(tag->name != NULL);
+    EXPECT_STREQ(tag->name, "br");
+    EXPECT_EQ(tag->attribute_count, 0);
+    EXPECT_TRUE(tag->attributes == NULL);
+
+    tag_destroy(tag);
+}
+
+TEST(TAG_CREATE_FROM_HTML, VALID_TAG_MULTIPLE_ATTRIBUTES) {
+    // Arrange
+    int err = OK;
+    const char* const html = "<img source=\"somepic.jpg\" width=\"800\" height=\"600\">";
+
+    // Act
+    tag* tag = tag_create_from_html(&err, html);
+
+    // Assert
+    EXPECT_TRUE(tag != NULL);
+    EXPECT_TRUE(tag->name != NULL);
+    EXPECT_STREQ(tag->name, "img");
+    EXPECT_EQ(tag->attribute_count, 3);
+    EXPECT_TRUE(tag->attributes != NULL);
+
+    EXPECT_STREQ(tag->attributes[0].name, "source");
+    EXPECT_STREQ(tag->attributes[0].value, "somepic.jpg");
+
+    EXPECT_STREQ(tag->attributes[1].name, "width");
+    EXPECT_STREQ(tag->attributes[1].value, "800");
+
+    EXPECT_STREQ(tag->attributes[2].name, "height");
+    EXPECT_STREQ(tag->attributes[2].value, "600");
+
+    tag_destroy(tag);
+}
+
