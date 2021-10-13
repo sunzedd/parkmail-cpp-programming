63568b84d1b6903ff0220296fa86202fce911f49
sunzedd <sunz89@yandex.ru>
2021-10-13 20:38:02 +0300

    Update main

--- homework_1/cmd/main.c
+++ homework_1/cmd/main.c
@@ -25,25 +25,32 @@ void print_tag(const tag_t *tag) {
 
 int main(void) {
     const char *const t1 = "<script type=\"module\" src=\"new_tab_page.js\">";
-    const char *const t2 = "<video width=\"320\" height=\"240\" controls>";
-    const char *const t3 = "asd";
+//    const char *const t2 = "HELLO";
+//    const char *const t3 = "<a>";
+//    const char *const t4 = "<>";
+//    const char *const t5 = "";
 
     int err = OK;
 
     tag_t *tag1 = tag_create_from_html(&err, t1);
-    printf("log error: %d\n", err);
-    tag_t *tag2 = tag_create_from_html(&err, t2);
-    printf("log error: %d\n", err);
-    tag_t *tag3 = tag_create_from_html(&err, t3);
-    printf("log error: %d\n", err);
+//    tag_t *tag2 = tag_create_from_html(&err, t2);
+//    tag_t *tag3 = tag_create_from_html(&err, t3);
+//    tag_t *tag4 = tag_create_from_html(&err, t4);
+//    tag_t *tag5 = tag_create_from_html(&err, t5);
+
+//    printf("log error: %d\n", err);
 
     print_tag(tag1);
-    print_tag(tag2);
-    print_tag(tag3);
+//    print_tag(tag2);
+//    print_tag(tag3);
+//    print_tag(tag4);
+//    print_tag(tag5);
 
-    tag_destroy(tag1);
-    tag_destroy(tag2);
-    tag_destroy(tag3);
+    tag_destroy(&tag1);
+//    tag_destroy(&tag2);
+//    tag_destroy(&tag3);
+//    tag_destroy(&tag4);
+//    tag_destroy(&tag5);
 
     return 0;
 }
