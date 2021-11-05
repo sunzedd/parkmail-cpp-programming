32c0aabd35295908b4d37f45aa71f3af1f5e3c9f
Rostislav V <sunz89@yandex.ru>
2021-11-05 22:11:52 +0300

    Remove extra files

--- homework_2/lib/reader_utils/include/reader_utils/test_func.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <stdio.h>
-
-void test_func( void );
--- homework_2/lib/reader_utils/src/test_func.c
+++
@@ -1,5 +0,0 @@
-#include "reader_utils/test_func.h"
-
-void test_func( void ) {
-    printf("test_func is called from reader_utils library\n");
-}
